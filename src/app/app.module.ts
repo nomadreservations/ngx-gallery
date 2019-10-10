b25d25aa66ba7472b7764a2dcb06ece923084703
Paul Ryan <paul@simplycomplex.co>
2019-10-10 09:43:26 -0600

    chore(): add demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Ng2PageScrollModule } from 'ng2-page-scroll';
 import { NgxGalleryModule } from '../../projects/gallery/src';
 import { AppComponent } from './app.component';
 import { OptionsViewerComponent } from './options-viewer/options-viewer.component';
 
 @NgModule({
   declarations: [AppComponent, OptionsViewerComponent],
-  imports: [BrowserModule, NgxGalleryModule, Ng2PageScrollModule],
+  imports: [BrowserModule, NgxGalleryModule],
   providers: [],
   bootstrap: [AppComponent]
 })
